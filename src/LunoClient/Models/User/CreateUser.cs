146a539458ec9c8f955935f345acb7656a71a48f
Alex Forbes-Reed <alexreed@outlook.com>
2016-02-13 21:47:18 +0000

    fixed bug on creating user. now it doesn't send anything if the field is blank, rather than null.

--- src/LunoClient/Models/User/CreateUser.cs
+++ src/LunoClient/Models/User/CreateUser.cs
@@ -5,28 +5,28 @@ namespace Luno.Models.User
 {
 	public class CreateUser<T>
 	{
-		[JsonProperty("email")]
+		[JsonProperty("email", NullValueHandling = NullValueHandling.Ignore)]
 		public string Email { get; set; }
 
-		[JsonProperty("name")]
+		[JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
 		public string Name { get; set; }
 
-		[JsonProperty("first_name")]
+		[JsonProperty("first_name", NullValueHandling = NullValueHandling.Ignore)]
 		public string FirstName { get; set; }
 
-		[JsonProperty("last_name")]
+		[JsonProperty("last_name", NullValueHandling = NullValueHandling.Ignore)]
 		public string LastName { get; set; }
 
-		[JsonProperty("username")]
+		[JsonProperty("username", NullValueHandling = NullValueHandling.Ignore)]
 		public string Username { get; set; }
 
-		[JsonProperty("created")]
+		[JsonProperty("created", NullValueHandling = NullValueHandling.Ignore)]
 		public DateTime? Created { get; set; }
 
-		[JsonProperty("profile")]
+		[JsonProperty("profile", NullValueHandling = NullValueHandling.Ignore)]
 		public T Profile { get; set; }
 
-		[JsonProperty("password")]
+		[JsonProperty("password", NullValueHandling = NullValueHandling.Ignore)]
 		public string Password { get; set; }
 	}
 }
