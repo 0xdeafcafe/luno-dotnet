0a8a81028e0813f856f64528791d43b758739d0d
Alex Forbes-Reed <alexreed@outlook.com>
2016-02-14 16:22:04 +0000

    library readed 0.1.0-alpha -- user client finished

--- src/LunoClient/Interfaces/IAnalyticsClient.cs
+++ src/LunoClient/Interfaces/IAnalyticsClient.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Luno.Interfaces
+﻿namespace Luno.Interfaces
 {
 	public interface IAnalyticsClient
 	{
