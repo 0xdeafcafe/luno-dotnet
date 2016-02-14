93b33f323d32f7d5689160b9f0b27fdd4a1f5538
Alex Forbes-Reed <alexreed@outlook.com>
2016-02-14 22:56:24 +0000

    fixed unit test bug

--- test/Luno.Test.LunoClient/Models/Test/Profile.cs
+++ test/Luno.Test.LunoClient/Models/Test/Profile.cs
@@ -4,13 +4,13 @@ namespace Luno.Test.LunoClient.Models.Test
 {
 	public class Profile
 	{
-		[JsonProperty("field_1", NullValueHandling = NullValueHandling.Ignore)]
+		[JsonProperty("field_1")]
 		public string Field1 { get; set; }
 
-		[JsonProperty("field_2", NullValueHandling = NullValueHandling.Ignore)]
+		[JsonProperty("field_2")]
 		public string Field2 { get; set; }
 
-		[JsonProperty("field_3", NullValueHandling = NullValueHandling.Ignore)]
+		[JsonProperty("field_3")]
 		public string Field3 { get; set; }
 	}
 }
