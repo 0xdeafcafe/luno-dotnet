af4a52961eb2434f546ea84248aa46170a9b794b
Alex Forbes-Reed <alexreed@outlook.com>
2016-03-03 13:33:22 +0000

    added a jsondatetimeconverter for Json.Net to perse datetimes to luno's preferred ISO format

---
+++ src/LunoClient/Converters/JsonDateTimeConveter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Luno.Converters
+{
+	internal class JsonDateTimeConverter : JsonConverter
+	{
+		public override bool CanConvert(Type objectType)
+		{
+			return (objectType == typeof(DateTime));
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			return DateTime.Parse(reader.Value.ToString());
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			writer.WriteValue(((value as Nullable<DateTime>)?.ToString("yyyy-MM-ddTHH:mm:ss.fff") + "Z"));
+		}
+	}
+}
